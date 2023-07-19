ecdff43baa45ecef21ef704feb8fe8dd390a74a5
Rebeca Vitória de Aguiar Coutinho <rebeca.coutinho@kempetro.com.br>
2023-07-19 12:10:04 -0300

    development dash sidebar

--- dashfrontend/src/App.js
+++ dashfrontend/src/App.js
@@ -1,32 +1,56 @@
 import React from 'react'
 import './App.css'
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {FiSettings} from 'react-icons/fi'
-import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 function App() {
+    const activeMenu = false;
+
   return (
 <div>
-
-    <div className="flex relative dark:bg-main-dark-bg">
-        <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
-            <TooltipComponent content="Settings" position='Top'>
-                <button type='button' 
-                className='text-3xl p-3 
-                hover:drop-shadow-xl
-                hover:bg-light-gray
-                text-white'
-                
-                style={{background: 'orange', borderRadius: '50%'}}>
-                    <FiSettings/>
-
+    <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+                <TooltipComponent content="Settings" position='Top'>
+                    <button type='button' 
+                    className='text-3xl p-3 
+                    hover:drop-shadow-xl
+                    hover:bg-light-gray
+                    text-white'
                     
-                </button>
-            </TooltipComponent>
+                    style={{background: 'orange', borderRadius: '50%'}}>
+                        <FiSettings/>
+
+                        
+                    </button>
+                </TooltipComponent>
+            </div>
+            {
+                activeMenu ? (
+
+                    <div className="w-72 fixed sidebar
+                    dark:bg-secondary-dark-bg
+                    bg-white">
+                        Sidebar
+                    </div>
+
+                 ) :(
+
+                    <div className='dark:bg-secondary-dark-bg'>
+                        Sidebar w-0
+                    </div>
+                 )}
+                 <div className={
+                    'dark:bg-main-bg bg-main-bg min-h-screen w-full' 
+                    + activeMenu ? 'md:ml-72' : 'flex-2'
+                    }>
+                        
+                   
+                 </div>
         </div>
-    </div>
-   
-  
+    </BrowserRouter>
 </div>
     
   )
