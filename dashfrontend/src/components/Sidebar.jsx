63c7898a3901738a69475fc382cd47d48509ebe6
rebecakemps <beca.cout@gmail.com>
2023-07-19 23:00:20 -0300

    nav and sidebar

---
+++ dashfrontend/src/components/Sidebar.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {links} from "../data/dummy"
+import { useStateContext } from '../contexts/ContextProvider';
+
+
+function Sidebar() {
+  const {activeMenu, setActiveMenu } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined ) {
+      setActiveMenu(false);
+    }
+  };
+
+  return (
+    <div className='ml-3 h-screen 
+    md:overflow-hidden overflow-auto 
+    md:hover:overflow-auto pb-10'>
+
+      {activeMenu && (
+        <>
+          <div className='flex justify-between
+          items-center'>
+            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 
+            ml-3 mt-4 flex text-xl font-extrabold 
+            tracking-tight dark:text-white text-slate-900">
+              <SiShopware /> <span>Kempetro</span>
+            </Link>
+            
+          </div>
+
+          <div className='mt-10'>
+            {links.map((item) => (
+              <div key={item.title} >
+                <p className='text-gray-400 m-3
+              mt-4 uppercase'>
+
+                  {item.title}
+                </p>
+                {item.links.map((link)=> (
+                  <NavLink to={link.link} 
+                  key={link.name}
+                  onClick={()=>{}}
+                   className='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-full text-md
+                    text-gray-700
+                    dark:text-gray-200
+                    dark:hover:text-blackhover:bg-light-gray m-2 hover:bg-orange-300'
+                  >
+                    {link.icon}
+                    <span className='capitalize'>{link.name}</span>
+                  </NavLink>
+                  
+                ))}
+              </div>
+            ))}
+
+          </div>
+        </>
+      ) }
+    </div>
+  )
+}
+
+export default Sidebar
