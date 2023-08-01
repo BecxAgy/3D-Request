c1a879d215604701290b079829547a1dd0ab676a
rebecakemps <beca.cout@gmail.com>
2023-07-31 22:36:33 -0300

    refactor

--- dashfrontend/src/components/index.jsx
+++ dashfrontend/src/components/index.jsx
@@ -2,9 +2,9 @@
 export { default as Sidebar } from './Sidebar';
 // eslint-disable-next-line import/no-cycle
 export { default as Navbar } from './Navbar';
-export {default as Header} from './Login/Header';
-export {default as Input} from './Login/Input'
-export {default as FormAction} from './Login/FormAction'
+export {default as Header} from './Auth/Header';
+export {default as Input} from './Auth/Input'
+export {default as FormAction} from './Auth/FormAction'
 export {default as CardMain} from './CardMain'
 export {default as HeaderMain} from './HeaderMain'
 
